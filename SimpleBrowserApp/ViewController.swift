1bb667356ed6dfac30ef40d2fc615b692597815a
Terashima <yamanogomora@gmail.com>
2019-08-24 10:41:42 +0900

    Edit: UI部品をコードに接続する

--- SimpleBrowserApp/ViewController.swift
+++ SimpleBrowserApp/ViewController.swift
@@ -7,9 +7,17 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var urlTextField: UITextField!
+    @IBOutlet weak var browserWebView: WKWebView!
+    
+    @IBOutlet weak var backButton: UIBarButtonItem!
+    @IBOutlet weak var forwardButton: UIBarButtonItem!
+    @IBOutlet weak var reloadButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func goBack(_ sender: Any) {
+    }
+    
+    @IBAction func goForward(_ sender: Any) {
+    }
+    
+    @IBAction func reload(_ sender: Any) {
+    }
 }
 
